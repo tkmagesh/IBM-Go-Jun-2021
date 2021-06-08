838a63f2fb79f613db3e39d5a471ff2d7b93d604
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-08 16:22:40 +0530

    closures assignment - solution

--- 03-functions/05-demo.go
+++ 03-functions/05-demo.go
@@ -3,16 +3,31 @@ package main
 import "fmt"
 
 //hide the counter variable in such a way that it is accessible only to the count function
-var counter int = 0
 
-func count() int {
-	counter += 1
-	return counter
+func getCount() (func() int, func() int) { // step 1
+	var counter int = 0 //step 2
+
+	increment := func() int { //step 3
+		counter += 1 //step 4
+		return counter
+	}
+
+	decrement := func() int {
+		counter -= 1
+		return counter
+	}
+
+	return increment, decrement // step 5
 }
 
 func main() {
-	fmt.Println(count())
-	fmt.Println(count())
-	fmt.Println(count())
-	fmt.Println(count())
+	increment, decrement := getCount()
+	fmt.Println(increment())
+	fmt.Println(increment())
+	fmt.Println(increment())
+	fmt.Println(increment())
+	fmt.Println(decrement())
+	fmt.Println(decrement())
+	fmt.Println(decrement())
+	fmt.Println(decrement())
 }
