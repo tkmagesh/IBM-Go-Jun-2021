d4493b5431dc8b871fcdb59f6e598ba740f5f8ec
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-08 12:15:07 +0530

    program structure and variable declarations

---
+++ 01-hello-world/hello-world.go
@@ -0,0 +1,67 @@
+//package declaration
+package main
+
+//import other packages
+import "fmt"
+
+//package level variables
+
+//package init function
+
+//main function
+func main() {
+	/*
+		var message string
+		message = "Hello World!"
+		fmt.Println(message)
+	*/
+
+	/*
+		var message string
+		var name string
+		message = "Hello"
+		name = "Magesh"
+	*/
+	/*
+		var (
+			message string
+			name    string
+		)
+		message = "Hello"
+		name = "Magesh"
+	*/
+
+	/*
+		var (
+			message, name string
+		)
+		message = "Hello"
+		name = "Magesh"
+	*/
+
+	/*
+		var message, name string
+		message = "Hello"
+		name = "Magesh"
+	*/
+
+	/*
+		var message string = "Hello"
+		var name string = "Magesh"
+	*/
+
+	/*
+		var message = "Hello"
+		var name = "Magesh"
+	*/
+
+	/*
+		var message, name = "Hello", "Magesh"
+	*/
+
+	//the following syntax is applicable only in a function (not at the package level)
+	message, name := "Hello", "Magesh"
+	fmt.Println(message, name)
+}
+
+//other functions
