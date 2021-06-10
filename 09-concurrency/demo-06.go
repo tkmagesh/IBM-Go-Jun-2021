7a926a302da252d9f1129c82e9f4594eba52d087
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-10 15:34:15 +0530

    assignment solution

--- 09-concurrency/demo-06.go
+++ 09-concurrency/demo-06.go
@@ -5,16 +5,21 @@ import (
 	"time"
 )
 
-func print(msg string, delay time.Duration) {
+func print(msg string, delay time.Duration, in chan string, out chan string) {
 	for i := 0; i < 5; i++ {
+		<-in
 		time.Sleep(delay)
 		fmt.Println(msg)
+		out <- "done"
 	}
 }
 
 func main() {
-	go print("Hello", 1*time.Second)
-	go print("World", 3*time.Second)
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	go print("Hello", 1*time.Second, ch1, ch2)
+	go print("World", 3*time.Second, ch2, ch1)
+	ch1 <- "start"
 	var input string
 	fmt.Scanln(&input)
 }
