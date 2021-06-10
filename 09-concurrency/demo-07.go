a62e1fa9dfd71bd06fb1fe251613e713ae469c5a
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-10 16:00:15 +0530

    divide & conquer - assignment

---
+++ 09-concurrency/demo-07.go
@@ -0,0 +1,20 @@
+/* divide & conquer */
+package main
+
+func sum(nos ...int) int {
+	result := 0
+	for _, no := range nos {
+		result += no
+	}
+	return result
+}
+
+func main() {
+	data := []int{4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55}
+	/*
+		split the data into two sets
+		add those two sets of data concurrently
+		get the results and add them to get the final total
+		display the final total
+	*/
+}
