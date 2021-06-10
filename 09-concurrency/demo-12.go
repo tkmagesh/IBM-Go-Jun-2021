995576159e392f80007b9f6285a9ebcbc42a74f1
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-10 17:17:30 +0530

    using timer apis

---
+++ 09-concurrency/demo-12.go
@@ -0,0 +1,37 @@
+//generate fixed number of fibonacci series
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func fibonacci(ch chan int) {
+	x, y := 0, 1
+	stop := time.After(10 * time.Second)
+	for {
+		select {
+		case ch <- x:
+			time.Sleep(500 * time.Millisecond)
+			x, y = y, x+y
+		case <-stop:
+			fmt.Println("Quitting")
+			close(ch)
+			return
+		}
+	}
+}
+
+func main() {
+	fmt.Println("Press ENTER to stop...")
+	ch := make(chan int)
+	go fibonacci(ch)
+	go func() {
+		for no := range ch {
+			fmt.Println(no)
+		}
+	}()
+	var input string
+	fmt.Scanln(&input)
+}
