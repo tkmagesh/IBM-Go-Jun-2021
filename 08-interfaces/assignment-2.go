3ad75306768ddc75d5e9b714124e20f217cd2f81
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-10 14:03:56 +0530

    assignment 2 - solution

--- 08-interfaces/assignment-2.go
+++ 08-interfaces/assignment-2.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	fmt.Println(sum(10, 20))                                                 //=> 30
@@ -14,6 +17,26 @@ func main() {
 	fmt.Println(sum(10, 20, []interface{}{30, 40, []interface{}{10, "20"}})) //=> 130
 }
 
-func sum( /*  */ ) int {
-
+func sum(nos ...interface{}) int {
+	result := 0
+	for _, val := range nos {
+		switch val.(type) {
+		case int:
+			result += val.(int)
+		case string:
+			if x, err := strconv.Atoi(val.(string)); err != nil {
+				result += x
+			}
+		case []int:
+			values := val.([]int)
+			intValList := make([]interface{}, len(values))
+			for idx, x := range values {
+				intValList[idx] = x
+			}
+			result += sum(intValList...)
+		case []interface{}:
+			result += sum(val.([]interface{})...)
+		}
+	}
+	return result
 }
