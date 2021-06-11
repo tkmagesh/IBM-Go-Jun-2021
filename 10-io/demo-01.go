a47225c613bca80ce419d36f7f56a70c96c68cc9
Magesh Kuppan <tkmagesh77@gmail.com>
2021-06-11 10:23:55 +0530

    custom 'Copy' implementation

--- 10-io/demo-01.go
+++ 10-io/demo-01.go
@@ -22,8 +22,27 @@ func (alphaReader AlphaReader) Read(buffer []byte) (int, error) {
 	return count, io.EOF
 }
 
+func Copy(writer io.Writer, reader io.Reader) (int64, error) {
+	var totalBytes int64
+	for {
+		buffer := make([]byte, 1024)
+		bytesRead, err := reader.Read(buffer)
+		totalBytes += int64(bytesRead)
+		if bytesRead > 0 {
+			_, writerErr := writer.Write(buffer)
+			if writerErr != nil {
+				return totalBytes, writerErr
+			}
+		}
+		if err != nil {
+			return totalBytes, err
+		}
+	}
+}
+
 func main() {
 	alphaReader := AlphaReader("Hi, How are you?")
 	io.Copy(os.Stdout, alphaReader)
+	/* Copy(os.Stdout, alphaReader) */
 	fmt.Println("Done")
 }
